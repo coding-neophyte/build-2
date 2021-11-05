09c578d86eea542eea57952719b2374112898dd7
Julius Franklin <jjfranklin88@outlook.com>
2021-11-05 13:33:19 -0400

    get test passing

--- index.test.js
+++ index.test.js
@@ -2,9 +2,9 @@ const getMethod = require('./index.js');
 
 describe('getMethod', () => {
   it('returns the method from a request', () => {
-    const Request = `GET / HTTP/1.1
-Host: example.com
-Accept-Language: us-en`;
+    const Request = `GET / HTTP/1.1\r
+Host: this.com\r
+Accept-Language: us-en \r`;
 
     const method = getMethod(Request);
 
